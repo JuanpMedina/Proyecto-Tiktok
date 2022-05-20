1803cc723674d50002c12a6687f1e93e799ce533
Juan Pablo Medina <Juampamedinadiaz2002@gmail.com>
2022-05-19 22:46:25 -0500

    Actualizacion terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
+      source = "hashicorp/aws"
     }
   }
 }
@@ -11,56 +10,64 @@ provider "aws" {
   region = "us-west-1"
 }
 
+resource "aws_instance" "app_server" {
+  ami           = "ami-0c4f7023847b90238"
+  instance_type = "t2.micro"
+
+  security_groups = [aws_security_group.allow_ssh.name]
+  key_name        = "tiktok-tf"
+
+  user_data = <<-EOF
+    #!/bin/bash
+    set -ex
+    sudo yum update -y
+    sudo yum install docker -y
+    sudo service docker start
+    sudo usermod -a -G docker ec2-user
+    sudo docker run -d -p 3000:3000 juanpmedina/prueba:main
+  EOF
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "tiktok-tf"
+  }
+}
+
+
 resource "aws_security_group" "allow_ssh" {
- name        = "allow_ssh"
- description = "Allow ssh inbound traffic"
- vpc_id      = "vpc-0ee576fd748b5f3a7"
-
- ingress {
-   description      = "SSH from VPC"
-   from_port        = 22
-   to_port          = 22
-   protocol         = "tcp"
-   cidr_blocks      = ["0.0.0.0/0"] 
- }
-
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
 
   ingress {
-    from_port = 3000
-    protocol = "tcp"
-    to_port = 3000
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "HTTP from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+   cidr_blocks      = ["0.0.0.0/0"] 
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
- 
- egress {
-   from_port        = 0
-   to_port          = 0
-   protocol         = "-1"
-   cidr_blocks      = ["0.0.0.0/0"]
- }
- 
- tags = {
-   Name = "allow_ssh"
- }
-}
 
-data "template_file" "userdata" {
-  template = file("${path.module}/instalacion.sh")
-}
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 3000
+    to_port          = 3000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-resource "aws_instance" "ec2_tiktok" {
-  ami = "ami-0c4f7023847b90238"
-  instance_type = "t2.micro"
-  key_name = "juanpmedina"
-  vpc_security_group_ids = [
-    aws_security_group.allow_ssh.id
-  ]
-  user_data = data.template_file.userdata.rendered
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   tags = {
-    Name = "EC2 tiktok"
+    Name = "allow_http"
   }
-
-
- 
 }
  
